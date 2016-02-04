8090c161a4a8fc35a4a5b1342386b4a5cb5556e7
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-02-04 00:48:21 -0700

    move dust code

--- globber/dust/core.py
+++ globber/dust/core.py
@@ -6,43 +6,6 @@ from scipy.ndimage import map_coordinates
 import pdb
 from astropy import wcs
 
-def get_red_fac(version=-1, ps=False, lsst=False): # dumb interface
-    self = get_red_fac
-    if (version == -1) and getattr(self, 'rf', None) is not None:
-        return self.rf
-    if version == 0:
-        rf = {'u':5.155, 'g':3.793, 'r':2.751, 'i':2.086, 'z':1.479, 'y':1.}
-    else:
-        #this is version from IDL
-        # that was trained up on blue tip stuff, forcing z band to agree with F99
-        # but getting all other information from blue tip
-        # we are replacing that here with the S10 F99-based table
-        #rf = {'u':4.292, 'g':3.286, 'r':2.282, 'i':1.714, 'z':1.266, 'y':1.}
-        rf  = {'u':4.239, 'g':3.303, 'r':2.285, 'i':1.698, 'z':1.263 }
-    if ps:
-        rf = {'g':3.172, 'r':2.271, 'i':1.682, 'z':1.322, 'y':1.087 }
-
-    return rf
-
-def set_red_fac(red_fac=None, mode=None):
-    if red_fac is not None and mode is not None:
-        raise ValueError('Must set only one of red_fac and mode')
-    if red_fac is None and mode is None:
-        raise ValueError('Must set one of red_fac and mode')
-    if red_fac is not None:
-        get_red_fac.rf = red_fac
-        return
-    if mode == 'lsst':
-        get_red_fac.rf = {'u':4.145, 'g':3.237, 'r':2.273, 'i':1.684,
-                          'z':1.323, 'y':1.088}
-    elif mode == 'ps':
-        get_red_fac.rf = {'g':3.172, 'r':2.271, 'i':1.682, 'z':1.322,
-                          'y':1.087}
-    elif mode == 'sdss':
-        get_red_fac.rf = get_red_fac(version=1)
-    else:
-        raise Exception('bug!')
-
 def getval(l, b, map='sfd', size=None, order=1):
     """Return SFD at the Galactic coordinates l, b.
 
