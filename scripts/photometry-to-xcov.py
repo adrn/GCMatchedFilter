9fe0d6861eeaf1869ad4d267a425687cefced95a
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-02-21 23:03:40 -0500

    overhaul to move all the suck to the top

--- scripts/photometry-to-xcov.py
+++ scripts/photometry-to-xcov.py
@@ -6,6 +6,7 @@ __author__ = "adrn <adrn@astro.columbia.edu>"
 
 # Standard library
 import os
+import sys
 
 # Third-party
 import astropy.coordinates as coord
@@ -14,21 +15,34 @@ import astropy.units as u
 import numpy as np
 import h5py
 
-# Global configuration stuff
+# -------------------------------------------------------------------
 # HACK: should be able to set this at command line?
 cluster_c = coord.SkyCoord(ra=229.352*u.degree,
                            dec=-21.01*u.degree)
 cluster_pad = {
-    'inner': 0.08*u.degree,
-    'outer': 0.2*u.degree
+    'inner': 7*u.arcmin,
+    'outer': 12*u.arcmin
 }
+search_region = dict(
+    ra=(224.,234),
+    dec=(-26,-16)
+)
+control_regions = [
+    dict(ra=(223,235), dec=(-16,-15)),
+    dict(ra=(223,235), dec=(-27,-26)),
+    dict(ra=(223,224), dec=(-26,-16)),
+    dict(ra=(234,235), dec=(-26,-16))
+]
+# -------------------------------------------------------------------
 
 def data_to_X_cov(data):
     X = np.vstack([data['dered_{}'.format(band)] for band in 'griz']).T
     Xerr = np.vstack([data['{}Err'.format(band)] for band in 'griz']).T
 
+    # HACK: this is fixed...colors should be customizable too, eh?
     # mixing matrix W
     W = np.array([[1, 0, 0, 0],    # g magnitude
+                  [0, 0, 1, 0],    # i magnitude
                   [1, -1, 0, 0],   # g-r color
                   [1, 0, -1, 0],   # g-i
                   [1, 0, 0, -1]])  # g-z
@@ -41,14 +55,14 @@ def data_to_X_cov(data):
     # each covariance C = WCW^T
     Xcov = np.tensordot(np.dot(Xcov, W.T), W, (-2, -1))
 
-    # HACK: slicing to ignore z
-    return X[:,:3], Xcov[:,:3,:3]
+    return X[:,1:], Xcov[:,1:,1:] # ignore g magnitude
 
-# HACK:
-def color_cut(X, lims):
-    idx = np.ones(X.shape[0]).astype(bool)
-    for i,(lo,hi) in enumerate(lims):
-        idx &= (X[:,i] > lo) & (X[:,i] < hi)
+def between(arr, lims):
+    return (arr >= lims[0]) & (arr < lims[1])
+
+def sky_cut(ra, dec, lims):
+    idx = ((ra > lims['ra'][0]) & (ra < lims['ra'][1]) &
+           (dec > lims['dec'][0]) & (dec < lims['dec'][1]))
     return idx
 
 def main(ps1_filename, out_filename=None, overwrite=False):
@@ -69,32 +83,51 @@ def main(ps1_filename, out_filename=None, overwrite=False):
 
     # Load PS1 photometry
     ps1 = np.load(ps1_filename)
-    ps1 = ps1[np.isfinite(ps1['dered_g']) & np.isfinite(ps1['dered_r']) &
-              np.isfinite(ps1['dered_i'])]
+    mask = np.ones(len(ps1)).astype(bool)
 
-    # HACK:
-    sky_ix = ((ps1['ra'] > 220.) & (ps1['ra'] < 240.) &
-              (ps1['dec'] > -32.) & (ps1['dec'] < -12.))
-    ps1 = ps1[sky_ix]
-    allX, allCov = data_to_X_cov(ps1)
-    col_ix = color_cut(allX,
-                       lims=[(17.,21.), (0,0.7), (0,0.8)])
-    ps1 = ps1[col_ix]
+    # only finite (non-nan or inf) values
+    mask &= np.isfinite(ps1['dered_g'])
+    mask &= np.isfinite(ps1['dered_r'])
+    mask &= np.isfinite(ps1['dered_i'])
+    mask &= np.isfinite(ps1['dered_z'])
+    ps1 = ps1[mask]
+
+    # cut out color region
+    color_idx = np.ones(len(ps1)).astype(bool)
+    color_idx &= between(ps1['dered_i'], (15,21))
+    color_idx &= between(ps1['dered_g']-ps1['dered_r'], (0,0.7))
+    color_idx &= between(ps1['dered_g']-ps1['dered_i'], (0,1))
+    color_idx &= between(ps1['dered_g']-ps1['dered_z'], (0,1))
+    ps1 = ps1[color_idx]
+    logger.debug("{} stars after color cuts".format(len(ps1)))
+
+    # cut out search region
+    search_idx = sky_cut(ps1['ra'], ps1['dec'], lims=search_region)
+    search_ps1 = ps1[search_idx]
+    logger.debug("{} stars in search field".format(len(search_ps1)))
+
+    # control fields
+    control_idx = np.zeros_like(search_idx).astype(bool)
+    for region in control_regions:
+        control_idx |= sky_cut(ps1['ra'], ps1['dec'], lims=region)
+    control_ps1 = ps1[control_idx]
+    logger.debug("{} stars in control fields".format(len(control_ps1)))
 
     # randomize ps1 array so i can do simple slicing later
-    np.random.shuffle(ps1)
+    np.random.shuffle(search_ps1)
+    np.random.shuffle(control_ps1)
 
     # output hdf5 file
     # TODO: right now, this saves multiple versions of the data because I SUCK
     with h5py.File(out_filename, mode='w') as f:
 
         # feature and covariance matrices for all stars
-        allX, allCov = data_to_X_cov(ps1)
-        g = f.create_group('all')
-        g.create_dataset('X', allX.shape, dtype='f', data=allX)
-        g.create_dataset('Cov', allCov.shape, dtype='f', data=allCov)
-        g.create_dataset('ra', ps1['ra'].shape, dtype='f', data=ps1['ra'])
-        g.create_dataset('dec', ps1['dec'].shape, dtype='f', data=ps1['dec'])
+        g = f.create_group('search')
+        search_X, search_Cov = data_to_X_cov(search_ps1)
+        g.create_dataset('X', search_X.shape, dtype='f', data=search_X)
+        g.create_dataset('Cov', search_Cov.shape, dtype='f', data=search_Cov)
+        g.create_dataset('ra', search_ps1['ra'].shape, dtype='f', data=search_ps1['ra'])
+        g.create_dataset('dec', search_ps1['dec'].shape, dtype='f', data=search_ps1['dec'])
 
         # define coordinates object for all stars
         ps1_c = coord.ICRS(ra=ps1['ra']*u.degree, dec=ps1['dec']*u.degree)
@@ -102,20 +135,20 @@ def main(ps1_filename, out_filename=None, overwrite=False):
         # feature and covariance matrices for cluster stars
         cluster_idx = ps1_c.separation(cluster_c) < cluster_pad['inner']
         cluster = ps1[cluster_idx]
+        logger.debug("{} stars in cluster".format(len(cluster)))
 
-        clusterX, clusterCov = data_to_X_cov(cluster)
+        cluster_X, cluster_Cov = data_to_X_cov(cluster)
         g = f.create_group('cluster')
-        g.create_dataset('X', clusterX.shape, dtype='f', data=clusterX)
-        g.create_dataset('Cov', clusterCov.shape, dtype='f', data=clusterCov)
+        g.create_dataset('X', cluster_X.shape, dtype='f', data=cluster_X)
+        g.create_dataset('Cov', cluster_Cov.shape, dtype='f', data=cluster_Cov)
 
         # feature and covariance matrices for NON-cluster stars
-        g = f.create_group('noncluster')
-        ncX = allX[~cluster_idx]
-        ncCov = allCov[~cluster_idx]
-        g.create_dataset('X', ncX.shape, dtype='f', data=ncX)
-        g.create_dataset('Cov', ncCov.shape, dtype='f', data=ncCov)
-        g.create_dataset('ra', ncX.shape[0:1], dtype='f', data=ps1[~cluster_idx]['ra'])
-        g.create_dataset('dec', ncX.shape[0:1], dtype='f', data=ps1[~cluster_idx]['dec'])
+        g = f.create_group('control')
+        control_X, control_Cov = data_to_X_cov(control_ps1)
+        g.create_dataset('X', control_X.shape, dtype='f', data=control_X)
+        g.create_dataset('Cov', control_Cov.shape, dtype='f', data=control_Cov)
+        g.create_dataset('ra', control_ps1['ra'].shape, dtype='f', data=control_ps1['ra'])
+        g.create_dataset('dec', control_ps1['dec'].shape, dtype='f', data=control_ps1['dec'])
 
         f.flush()
         logger.debug("Saved {}".format(out_filename))
