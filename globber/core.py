8495d5e359c2d80a51aefc3a7643ad4980b028ab
Adrian Price-Whelan <adrian.prw@gmail.com>
2016-02-25 18:04:34 -0500

    added isochrone func

--- globber/core.py
+++ globber/core.py
@@ -4,6 +4,7 @@ __author__ = "adrn <adrn@astro.columbia.edu>"
 
 # Third-party
 import numpy as np
+from scipy.interpolate import splprep, splev
 
 def ps1_data_to_X_cov(data, W):
     """
@@ -26,3 +27,18 @@ def ps1_data_to_X_cov(data, W):
     V = np.einsum('lk,nmk->nml', W, V)
 
     return X, V
+
+def ps1_isoc_to_XCov(data, W, interpolate=False, n_interpolate=1024):
+    """
+    Given an isochrone in Pan-STARRS1 filters -- which must contain keys like
+    'gP1', etc. -- and a mixing matrix, `W`, create a feature / data matrix `X`.
+    """
+    X = np.vstack([data['{}P1'.format(band)] for band in 'grizy']).T
+    X = np.einsum('nj,mj->nm', X, W)
+
+    if interpolate:
+        tck, u = splprep(X[:,1:].T, u=X[:,0], k=3, s=1E-4)
+        u_fine = np.linspace(u.min(), u.max(), n_interpolate)
+        X = np.vstack([u_fine] + splev(u_fine, tck)).T
+
+    return X
